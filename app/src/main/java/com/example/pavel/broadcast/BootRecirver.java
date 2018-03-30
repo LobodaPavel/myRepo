f615e8835cdc0f4039820ff094d0cd9efce14a20
LobodaPavel <loboda.pavel.and@gmaail.com>
2018-03-30 22:42:02 +0300

    dfsdf

---
+++ app/src/main/java/com/example/pavel/broadcast/BootRecirver.java
@@ -0,0 +1,25 @@
+package com.example.pavel.broadcast;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BootRecirver extends BroadcastReceiver{
+
+    private static final String TAG = "myLogs";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_LONG).show();
+            for (int y=0; y<10; y++){
+                Log.d(TAG, "Boot Completed"); // Чтоб не потерять
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pavel/broadcast/MyService.java
@@ -0,0 +1,40 @@
+package com.example.pavel.broadcast;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    public MyService() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Have a nice day!!", Toast.LENGTH_LONG).show();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service has been stopped", Toast.LENGTH_SHORT).show();
+    }
+}
